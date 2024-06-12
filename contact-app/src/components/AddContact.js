b2037e3dfc7cb82b42ff426cf61b688d233e25df
Rohit Pal <rohitpal210@gmail.com>
2024-06-12 22:03:55 +0530

    message

---
+++ contact-app/src/components/AddContact.js
@@ -0,0 +1,35 @@
+import React  from "react";
+
+class AddContact extends React.Component {
+    render(){
+        return (
+            <div className="ui segment">
+                <h2></h2>
+                <h2>Add Contact</h2>
+                <form className="ui form">
+                    <div className="field">
+                        <label>
+                            Name
+                        </label>
+
+                        <input type="text" name="name" placeholder="Name"></input>
+                    </div>
+
+                    <div className="field">
+                        <label>
+                            Email
+                        </label>
+
+                        <input type="text" name="Email" placeholder="Email"></input>
+                    </div>
+
+                    <button className="ui button blue">Add</button>
+                </form>
+
+            </div>
+        );
+    }
+}
+
+export default AddContact;
+
