bd3b5b2db94053164d69e75765b999e39566650a
Rohit Pal <rohitpal210@gmail.com>
2024-06-13 10:30:06 +0530

    Your commit message here

--- contact-app/src/components/AddContact.js
+++ contact-app/src/components/AddContact.js
@@ -1,18 +1,40 @@
 import React  from "react";
 
 class AddContact extends React.Component {
+    state = {
+        name: "",
+        email: ""
+    };
+
+    add = (e) =>{
+        e.preventDefault();
+        if( this.state.name === "" || this.state.email === ""){
+            alert("All the field are mandatory!");
+            return
+        }
+        this.props.addContactHandler(this.state);
+        this.setState({name: "", email: ""});
+        
+    }
+
     render(){
+
+       
         return (
             <div className="ui segment">
                 <h2></h2>
                 <h2>Add Contact</h2>
-                <form className="ui form">
+                <form className="ui form" onSubmit={this.add}>
                     <div className="field">
                         <label>
                             Name
                         </label>
 
-                        <input type="text" name="name" placeholder="Name"></input>
+                        <input 
+                        type="text" name="name" 
+                        placeholder="Name"
+                        value = {this.state.name}
+                        onChange={(e)=> this.setState({name: e.target.value})}></input>
                     </div>
 
                     <div className="field">
@@ -20,7 +42,9 @@ class AddContact extends React.Component {
                             Email
                         </label>
 
-                        <input type="text" name="Email" placeholder="Email"></input>
+                        <input type="text" name="Email" placeholder="Email"
+                        value = {this.state.email}
+                        onChange={(e)=> this.setState({email: e.target.value})}></input>
                     </div>
 
                     <button className="ui button blue">Add</button>
--- contact-app/src/components/App.js
+++ contact-app/src/components/App.js
@@ -1,16 +1,24 @@
 import './App.css';
-import React from "react";
+import React, { useState } from "react";
 import Header from './Header';
 import AddContact from './AddContact';
 import ContactList from './ContactList';
 
 function App() {
+
+   const [contacts, setContacts] = useState([]);
+   
+
+   const addContactHandler = (contact) => {
+    console.log(contact);
+    setContacts([...contacts, contact]);
+   }
   return (
 
     <div>
       <Header />
-      <AddContact />
-      {/*<ContactList />*/}
+      <AddContact addContactHandler={addContactHandler}/>
+      <ContactList contacts={contacts}/>
     </div>
   
   );
