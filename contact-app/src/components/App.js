94f8d50646cc6cc2a2795c35c3d07ac8f8cd348c
Rohit Pal <rohitpal210@gmail.com>
2024-06-13 11:43:06 +0530

    push

--- contact-app/src/components/App.js
+++ contact-app/src/components/App.js
@@ -1,21 +1,39 @@
 import './App.css';
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from './Header';
 import AddContact from './AddContact';
 import ContactList from './ContactList';
 
 function App() {
-
+   
+   const LOCAL_STORAGE_KEY = "contacts";
    const [contacts, setContacts] = useState([]);
    
 
    const addContactHandler = (contact) => {
     console.log(contact);
     setContacts([...contacts, contact]);
-   }
+   };
+
+   useEffect(()=> {
+    
+    const retrieveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    console.log("Retrieved contacts:", retrieveContacts);
+
+    if(retrieveContacts){
+      setContacts(retrieveContacts);
+    
+    }
+   },[]);
+
+   useEffect(()=> {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+   },[contacts]);
+
+ 
   return (
 
-    <div>
+    <div className='ui container'>
       <Header />
       <AddContact addContactHandler={addContactHandler}/>
       <ContactList contacts={contacts}/>
